49ff717d10e1177393dddd67a656d85cf8e159fa
xaerr0 <dannybrowbrow@gmail.com>
2022-04-21 19:17:32 -0700

    Exercise 5

--- src/labs_examples/fundamentals/labs/Exercise_05.java
+++ src/labs_examples/fundamentals/labs/Exercise_05.java
@@ -1,6 +1,10 @@
 package labs_examples.fundamentals.labs;
 
 
+import org.w3c.dom.ls.LSOutput;
+
+import java.sql.SQLOutput;
+
 /**
  * Fundamentals Exercise 5: Working with Strings
  *
@@ -27,22 +31,36 @@ public class Exercise_05 {
 
 
         // please concatenate str & str2 and set the result to a new String variable below
-        String str3 = str+str2;
+        String str3 = str + str2;
         System.out.println(str3);
 
         // please demonstrate the use of any other method that is available to us in the String class
         // for example, replace(), substring(), contains(), indexOf() etc
 
+        // replace()
         String myStr = "Hello World!";
         System.out.println(myStr.replace('l', 'r'));
 
+        // substring()
+        String s1 = "CodingNomads";
+        String substr = s1.substring(6, 12);
+        System.out.println(substr);
 
+        // contains()
+        String s2 = "The Hunt for Red October";
+        if (s2.contains("Blue")) {
+            System.out.println("This string contains Blue");
+        } else
+            System.out.println("This string does not contain Blue");
 
-
-
-
-
+        // index0f()
+        String s3 = "This is a test!";
+        System.out.println(s3.indexOf("e",5));
     }
+
+
+
+
 
 
 }
