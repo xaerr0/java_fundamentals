480af8f6d1315bbee03d9f9dfa145a802100977c
xaerr0 <dannybrowbrow@gmail.com>
2022-05-09 19:34:59 -0700

    Array Practice

--- src/labs_examples/conditions_loops/labs/TraditionalArray.java
+++ src/labs_examples/conditions_loops/labs/TraditionalArray.java
@@ -1,2 +1,32 @@
-package labs_examples.conditions_loops.labs;public class TraditionalArray {
+package labs_examples.conditions_loops.labs;
+
+import java.util.Arrays;
+
+public class TraditionalArray {
+    public static void main(String[] args) {
+
+
+
+        int [] array = new int [10];
+        array[0] = 5;
+        array[0] = 0;
+
+
+        System.out.println(array.length);
+        System.out.println(array[0]);
+        System.out.println(array[7]);
+        System.out.println(array);
+
+        printArray(array);
+
+
+    }
+
+
+    private static void printArray(int[] table) {
+        for(int i = 0; i < table.length; i++) {
+            System.out.print(table[i] + " | ");
+        }
+    }
 }
+
