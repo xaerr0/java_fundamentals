61d86e0be73c88afd0e3bc76ace37fdc9b26e2ba
xaerr0 <dannybrowbrow@gmail.com>
2022-05-09 19:34:33 -0700

    exercises

--- src/labs_examples/conditions_loops/labs/Exercise_07.java
+++ src/labs_examples/conditions_loops/labs/Exercise_07.java
@@ -15,20 +15,28 @@ package labs_examples.conditions_loops.labs;
 
 public class Exercise_07 {
     public static void main(String[] args) {
-        int a;
-        int b;
 
-        for (a = 1; a <= 10; ++a)
-        {
-            for (b = 1; b <=10; ++b)
-            {
-                System.out.print(a * b+ " | ");
-            }
-            System.out.println();
+        String str = "Hello";
+
+
+
+
+
+
         }
 
 
 
 
-    }
-    }
+        }
+
+
+
+
+
+
+
+
+
+
+
